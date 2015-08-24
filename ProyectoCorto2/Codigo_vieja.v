f1977430ffa752a505d19c63f0c617f09c4a9f1c
jossCr <herrera20.1994@gmail.com>
2015-08-23 21:10:07 -0600

    Version1

---
+++ ProyectoCorto2/Codigo_vieja.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    21:09:25 08/23/2015 
+// Design Name: 
+// Module Name:    Codigo_vieja 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Codigo_vieja(
+    );
+
+
+endmodule
